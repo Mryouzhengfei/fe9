89cd9b9bbe776b0ff90da1738fc7cdce6d365031
Mryouzhengfei <1252048513@qq.com>
2017-09-26 19:02:44 +0800

    9.26提交

---
+++ learnvue/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <router-link to="/" tag="li" exact> 首页 </router-link>
+    <router-link to="/daily" tag="div" active-class="starkwang"> <a> 知乎日报</a> </router-link>
+    <router-link to="/blog" tag="div" active-class="starkwang"> <a> 博客</a> </router-link>
+    <router-link to="/work" tag="div" active-class="starkwang"> <a> 工作页面</a> </router-link>
+    <router-link to="/study" > <a> 学习页面</a> </router-link>
+    <router-link to="/about" > <a> 关于我</a> </router-link>
+
+    <!-- <a href="/">首页</a>
+    <a href="/work">工作页面</a>
+    <a href="/study">学习页面</a> -->
+    <span>Hi I'm stark wang</span>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.router-link-active{
+  background-color: rosybrown;
+}
+.starkwang{
+  background-color: blue;
+}
+.shudong{
+  background-color: yellowgreen;
+}
+</style>
---
+++ readme.md
@@ -0,0 +1,43 @@
+自我介绍：
+    王树东 stark.wang
+    github:wsdo
+
+    
+
+课程规划：
+    vue base
+    指令
+    模版
+    自定义指令
+
+
+    请求api的方式
+        ajax
+        resource 可以直接使用jsonp get post
+        axios get post 不能直接使用jsonp 需要安装jsonp模块 
+
+    vue项目
+
+
+        技术栈：   
+        vuejs2.0 
+        nodejs
+        express
+        mongondb
+        redis
+        jwt
+        passport
+        nginx
+
+    第一个阶段
+        express 简单的业务逻辑
+    第二阶段
+        完全是在企业中应用 （来重构第一个阶段讲的）
+
+    最后上线
+    在Linux 那个nginx
+    解决不同端口跨域
+
+课程视频永久地址
+如果地址改变，请记得来这里看哦
+链接: https://pan.baidu.com/s/1hsOThas 密码: rtbu
